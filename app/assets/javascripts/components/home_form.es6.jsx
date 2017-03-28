8ca48b1709ce0334e0c4023998efb1b829543a83
Yoshi Sukeda <yoshiaki_ssss@yahoo.com>
2017-03-28 11:05:44 +0900

    prevent form submission

--- app/assets/javascripts/components/home_form.es6.jsx
+++ app/assets/javascripts/components/home_form.es6.jsx
@@ -2,7 +2,7 @@ class HomeForm extends React.Component {
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <input name='prefectures' placeholder="Type in any prefecture..."
           autoFocus
           onChange={(e) => this.props.onUserInput(e.target.value)} />
